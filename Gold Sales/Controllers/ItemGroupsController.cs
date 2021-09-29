3786ea7090b39571ed5a077de102a1ce80fefcec
OmniaHussein25 <91592888+OmniaHussein25@users.noreply.github.com>
2021-09-29 10:35:38 +0200

    V1
    first commit

---
+++ Gold Sales/Controllers/ItemGroupsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Gold_Sales.Models;
+
+namespace Gold_Sales.Controllers
+{
+    public class ItemGroupsController : Controller
+    {
+        private Gold_SalesEntities db = new Gold_SalesEntities();
+
+        // GET: ItemGroups
+        public ActionResult Index()
+        {
+            return View(db.ItemGroups.ToList());
+        }
+
+        // GET: ItemGroups/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemGroup itemGroup = db.ItemGroups.Find(id);
+            if (itemGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemGroup);
+        }
+
+        // GET: ItemGroups/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ItemGroups/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemGroupID,ItemGroupname,active,rowcreateddate,MachineIP,MachineName,MachineUser,userid,rowupdateddate,Machineipupdated,Machinenameupdated,Machineuserupdated,useridupdated,iabc,idef,ighi,ijkl,Sabc,Sdef,Sghi,Sjkl,Dabc,Ddef,Dghi,Djkl,DECabc,DECdef,DECghi,DECjkl")] ItemGroup itemGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ItemGroups.Add(itemGroup);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(itemGroup);
+        }
+
+        // GET: ItemGroups/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemGroup itemGroup = db.ItemGroups.Find(id);
+            if (itemGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemGroup);
+        }
+
+        // POST: ItemGroups/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemGroupID,ItemGroupname,active,rowcreateddate,MachineIP,MachineName,MachineUser,userid,rowupdateddate,Machineipupdated,Machinenameupdated,Machineuserupdated,useridupdated,iabc,idef,ighi,ijkl,Sabc,Sdef,Sghi,Sjkl,Dabc,Ddef,Dghi,Djkl,DECabc,DECdef,DECghi,DECjkl")] ItemGroup itemGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(itemGroup).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(itemGroup);
+        }
+
+        // GET: ItemGroups/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemGroup itemGroup = db.ItemGroups.Find(id);
+            if (itemGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemGroup);
+        }
+
+        // POST: ItemGroups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ItemGroup itemGroup = db.ItemGroups.Find(id);
+            db.ItemGroups.Remove(itemGroup);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
