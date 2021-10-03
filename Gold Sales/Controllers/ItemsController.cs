3e8be3dd5c0c0eb7c8fa251187c64ae53bc3e449
OmniaHussein25 <91592888+OmniaHussein25@users.noreply.github.com>
2021-10-03 17:57:05 +0200

    Omnia

---
+++ Gold Sales/Controllers/ItemsController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Gold_Sales.Models;
+
+namespace Gold_Sales.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private Gold_SalesEntities db = new Gold_SalesEntities();
+
+        // GET: Items
+        public ActionResult Index()
+        {
+            var items = db.Items.Include(i => i.ItemGroup).Include(i => i.ItemUnit);
+            return View(items.ToList());
+        }
+
+        // GET: Items/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // GET: Items/Create
+        public ActionResult Create()
+        {
+            ViewBag.itemgroupid = new SelectList(db.ItemGroups, "ItemGroupID", "ItemGroupname");
+            ViewBag.itemunitcode = new SelectList(db.ItemUnits, "itemunitcode", "itemunitname");
+            return View();
+        }
+
+        // POST: Items/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemID,CompanyID,itemname,itemname_ENG,itemunitcode,ItemTaxCode,TaxCodeType,ItemTaxUnit,itemprice,itemcost,itemgroupid,itembarcode,main_vendor,active,rowcreateddate,MachineIP,MachineName,MachineUser,userid,rowupdateddate,Machineipupdated,Machinenameupdated,Machineuserupdated,useridupdated,iabc,idef,ighi,ijkl,Sabc,Sdef,Sghi,Sjkl,Dabc,Ddef,Dghi,Djkl,DECabc,DECdef,DECghi,DECjkl")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.itemgroupid = new SelectList(db.ItemGroups, "ItemGroupID", "ItemGroupname", item.itemgroupid);
+            ViewBag.itemunitcode = new SelectList(db.ItemUnits, "itemunitcode", "itemunitname", item.itemunitcode);
+            return View(item);
+        }
+
+        // GET: Items/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.itemgroupid = new SelectList(db.ItemGroups, "ItemGroupID", "ItemGroupname", item.itemgroupid);
+            ViewBag.itemunitcode = new SelectList(db.ItemUnits, "itemunitcode", "itemunitname", item.itemunitcode);
+            return View(item);
+        }
+
+        // POST: Items/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemID,CompanyID,itemname,itemname_ENG,itemunitcode,ItemTaxCode,TaxCodeType,ItemTaxUnit,itemprice,itemcost,itemgroupid,itembarcode,main_vendor,active,rowcreateddate,MachineIP,MachineName,MachineUser,userid,rowupdateddate,Machineipupdated,Machinenameupdated,Machineuserupdated,useridupdated,iabc,idef,ighi,ijkl,Sabc,Sdef,Sghi,Sjkl,Dabc,Ddef,Dghi,Djkl,DECabc,DECdef,DECghi,DECjkl")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.itemgroupid = new SelectList(db.ItemGroups, "ItemGroupID", "ItemGroupname", item.itemgroupid);
+            ViewBag.itemunitcode = new SelectList(db.ItemUnits, "itemunitcode", "itemunitname", item.itemunitcode);
+            return View(item);
+        }
+
+        // GET: Items/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Items/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(string id)
+        //{
+        //    Item item = db.Items.Find(id);
+        //    db.Items.Remove(item);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+        public JsonResult DeleteObj(string id)
+        {
+            bool res = false;
+            Item item = db.Items.Find(id);
+            if (item != null)
+            {
+                db.Items.Remove(item);
+                db.SaveChanges();
+                res = true;
+            }
+            return Json(res, JsonRequestBehavior.AllowGet);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
